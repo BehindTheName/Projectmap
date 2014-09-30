234ccd5e4f32f15578c372396b78c03810a1c06d
Mizzcoollizz <0891435@hr.nl>
2014-09-30 10:07:07 +0200

    Slide

---
+++ ActiesSlide.js
@@ -0,0 +1,19 @@
+var ReclameIMG = new Array();
+
+ReclameIMG[0] = new Image();
+ReclameIMG[0].src = 'ReclameKoffie.jpg';
+
+ReclameIMG[1] = new Image();
+ReclameIMG[1].src = 'ReclameActieMaand.jpg';
+
+ReclameIMG[2] = new Image();
+ReclameIMG[2].src = 'images/img/Splash_image3.jpg';
+
+//ReclameIMG[3] = new Image();
+//ReclameIMG[3].src = 'images/img/Splash_image4.jpg';
+
+//ReclameIMG[4] = new Image();
+//ReclameIMG[4].src = 'images/img/Splash_image5.jpg';
+
+//ReclameIMG[5] = new Image();
+//ReclameIMG[5].src = 'images/img/Splash_image6.jpg';
