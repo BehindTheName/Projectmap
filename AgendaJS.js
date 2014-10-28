43866ea6c208e1b31a225bebf5beaa2a81c6ee63
Mizzcoollizz <0891435@hr.nl>
2014-10-28 19:56:06 +0100

    :)

--- AgendaJS.js
+++ AgendaJS.js
@@ -45,13 +45,13 @@ var WelkeTijd;
 var Dagen = [
 //Zondag
 [
-[["How To Train Your Dragon", "Zaal 1"], ["X-men", "Zaal 2"], ["Let's Be Cops", "Zaal 3"]],//9:00
+[["How To Train Your Dragon", "Zaal 1"], ["X-men", "Zaal 2"], ["Let&#39;s Be Cops", "Zaal 3"]],//9:00
 [["If I Stay", "Zaal 1"], ["How To Train Your Dragon", "Zaal 2"], ["If I Stay", "Zaal 3"]],//12:00
-[["Let's Be Cops", "Zaal 1"], ["The Expandables 3", "Zaal 2"], ["The Maze Runner", "Zaal 3"]],//15:00
+[["Let&#39;s Be Cops", "Zaal 1"], ["The Expandables 3", "Zaal 2"], ["The Maze Runner", "Zaal 3"]],//15:00
 [["The Maze Runner", "Zaal 1"], ["Into The Strom", "Zaal 2"], ["Into The Storm", "Zaal 3"]],//16:00
 [["The Expandables", "Zaal 1"], ["The Maze Runner", "Zaal 2"], ["The Expandables", "Zaal 3"]],//18:00
 [["X-men", "Zaal 1"], ["If I Stay", "Zaal 2"], ["How To Train Your Dragon", "Zaal 3"]],//20:00
-[["Into The Storm", "Zaal 1"], ["Let's Be Cops", "Zaal 2"], ["X-men", "Zaal 3"]]//22:00
+[["Into The Storm", "Zaal 1"], ["Let&#39;s Be Cops", "Zaal 2"], ["X-men", "Zaal 3"]]//22:00
 ],
 
 //Maandag
@@ -81,9 +81,9 @@ var Dagen = [
 [
 [["How To Train Your Dragon", "Zaal 1"], ["Guardians Of The Galaxy", "Zaal 2"], ["If I Stay", "Zaal 3"]],//9:00
 [["Edge Of Tomorrow", "Zaal 1"], ["How To Train Your Dragon", "Zaal 2"], ["Edge OF Tomorrow", "Zaal 3"]],//12:00
-[["If I Stay", "Zaal 1"], ["Let's Be Cops", "Zaal 2"], ["The Maze Runner", "Zaal 3"]],//15:00
+[["If I Stay", "Zaal 1"], ["Let&#39;s Be Cops", "Zaal 2"], ["The Maze Runner", "Zaal 3"]],//15:00
 [["The Maze Runner", "Zaal 1"], ["Into The Storm", "Zaal 2"], ["Into The Storm", "Zaal 3"]],//16:00
-[["Let's Be Cops", "Zaal 1"], ["The Maze Runner", "Zaal 2"], ["Let's Be Cops", "Zaal 3"]],//18:00
+[["Let&#39;s Be Cops", "Zaal 1"], ["The Maze Runner", "Zaal 2"], ["Let&#39;s Be Cops", "Zaal 3"]],//18:00
 [["Guardians Of The Galaxy", "Zaal 1"], ["Edge Of Tomorrow", "Zaal 2"], ["How To train Your Dragon", "Zaal 3"]],//20:00
 [["Into The Storm", "Zaal 1"], ["If I Stay", "Zaal 2"], ["Guardians Of The Galaxy", "Zaal 3"]]//22:00
 ],
@@ -102,12 +102,12 @@ var Dagen = [
 
 //Vrijdag
 [
-[["Let's Be Cops", "Zaal 1"], ["How To Train Your Dragon", "Zaal 2"], ["Into The Storm", "Zaal 3"]],//9:00
-[["The Equalizer", "Zaal 1"], ["Let's Be Cops", "Zaal 2"], ["The Equalizer", "Zaal 3"]],//12:00
+[["Let&#39;s Be Cops", "Zaal 1"], ["How To Train Your Dragon", "Zaal 2"], ["Into The Storm", "Zaal 3"]],//9:00
+[["The Equalizer", "Zaal 1"], ["Let&#39;s Be Cops", "Zaal 2"], ["The Equalizer", "Zaal 3"]],//12:00
 [["Into The Storm", "Zaal 1"], ["X-Men", "Zaal 2"], ["The Expandables 3", "Zaal 3"]],//15:00
 [["The Expandables 3", "Zaal 1"], ["A Most Wanted Man", "Zaal 2"], ["A Most Wanted Man", "Zaal 3"]],//16:00
 [["X-Men", "Zaal 1"], ["The Expandables 3", "Zaal 2"], ["X-Men", "Zaal 3"]],//18:00
-[["How To Train Your Dragon", "Zaal 1"], ["The Equalizer", "Zaal 2"], ["Let's Be Cops", "Zaal 3"]],//20:00
+[["How To Train Your Dragon", "Zaal 1"], ["The Equalizer", "Zaal 2"], ["Let&#39;s Be Cops", "Zaal 3"]],//20:00
 [["A Most Wanted Man", "Zaal 1"], ["Into The Storm", "Zaal 2"], ["How To Train Your Dragon", "Zaal 3"]]//22:00
 ],
 
